cefba86045cab0626a544a366c23212b07f14255
zumaku <60200121101@uin-alauddin.ac.id>
2024-02-11 13:43:57 +0800

    Printing table

---
+++ playground/read_table/readtbl.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import sys
+
+def read_excel_table(file_name):
+    try:
+        df = pd.read_excel(file_name)
+        return df
+    except Exception as e:
+        print("Error reading Excel file:", e)
+        return None
+
+def main():
+    file_name = 'temp.xlsx'
+    
+    if len(sys.argv) != 3 or sys.argv[1] != '-t' or sys.argv[2] != 'all':
+        print("Usage: python readtbl.py -t all")
+        sys.exit(1)
+
+    table = read_excel_table(file_name)
+    if table is not None:
+        print(table.to_string(index=False))  # Print all attributes without index
+
+if __name__ == "__main__":
+    main()
